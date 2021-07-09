04f9c4ffe290fe543314b64f0598f0c00a695691
0522chan <0522chan@naver.com>
2021-07-09 09:02:16 +0900

    Try1

---
+++ BMI/BMI/Command.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BMI.Command
+{
+    public class Commands : ICommand
+    {
+        Action<object> _executeMethod;
+        Func<object, bool> _canexecuteMethod;
+
+        public Commands(Action<object> executeMethod, Func<object, bool> canexecuteMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canexecuteMethod = canexecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod(parameter);
+        }
+    }
+
+}
---
+++ BMI/BMI/ViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using BMI.Model;
+using BMI.Command;
+
+namespace BMI.ViewModel
+{
+    //INotifyPropertyChanged 인터페이스 구현(속성 변경 이벤트)
+    public class Notifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+    //코드 알림 속성  
+    public class ConverterViewModel : Notifier
+    {
+        Person _person = new Person();
+        public double Weight
+        {
+            get { return _person.Weight; }
+            set
+            {
+                _person.Weight = value;
+                OnPropertyChanged("Weight");
+
+
+            }
+        }
+        public double Tall
+        {
+            get { return _person.Tall; }
+            set
+            {
+                _person.Tall = value;
+                OnPropertyChanged("Tall");
+            }
+        }
+        public string Result
+        {
+            get { return _person.Result; }
+            set
+            {
+                _person.Result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+        private void OnResultChanged()
+        {
+            const double num = 0.0001;
+            double rate = Weight / ((Tall * Tall) * num);
+
+            if (rate <= 18.5)
+            {
+                Result = "저체중";
+            }
+            else if (rate > 18.5 && rate <= 22.9)
+            {
+                Result = "정상";
+            }
+            else if (rate >= 23.0 && rate <= 24.9)
+            {
+                Result = "과체중";
+            }
+            else if (rate >= 25.0)
+            {
+                Result = "비만";
+            }
+        }
+        //ICommand 
+        public ICommand DelegateCommand { get; set; }
+
+        public ConverterViewModel()
+        {
+            DelegateCommand = new Commands(ExecuteMethod, CanExecuteMethod);
+        }
+
+        private void ExecuteMethod(object obj)
+        {
+            OnResultChanged();
+        }
+
+        private bool CanExecuteMethod(object arg)
+        {
+            return true;
+        }
+    }
+}
